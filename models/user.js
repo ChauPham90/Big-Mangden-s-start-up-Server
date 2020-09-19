ac6f8811d168d1f6cbad7d7e6722a70c254d96bc
ChauPham90 <minhchaupham90@gmail.com>
2020-09-19 15:47:20 +0700

    merge User-signIn and deleted that branch

--- models/user.js
+++ models/user.js
@@ -51,13 +51,10 @@ userSchema
   });
 
 userSchema.methods = {
-<<<<<<< HEAD
-=======
   authenticate: function (password) {
     return this.encryptPassword(password) === this.hashed_password;
   },
 
->>>>>>> 65c70024afa43547c4cb93c23b5a94c68256b066
   encryptPassword: function (password) {
     if (!password) return "";
     try {
