b160054f163b3f46e8008f5755a0d2da0325246f
ChauPham90 <minhchaupham90@gmail.com>
2020-09-25 19:19:36 +0700

    added deleteProduct route

--- controllers/product.js
+++ controllers/product.js
@@ -23,6 +23,19 @@ exports.readProduct = (req, res) => {
   req.product.photo = undefined;
   return res.json(req.product);
 };
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: "Product deleted successfully",
+    });
+  });
+};
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
--- routes/product.js
+++ routes/product.js
@@ -4,10 +4,16 @@ const { userById } = require("../controllers/user");
 const { productById, readProduct } = require("../controllers/product");
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { create } = require("../controllers/product");
+const { create, remove } = require("../controllers/product");
 router.get("/product/:productId", readProduct);
 router.post("/product/create/:userId", requireSignin, isAdmin, isAuth, create);
-
+router.delete(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAdmin,
+  isAuth,
+  remove
+);
 router.param("userId", userById);
 router.param("productId", productById);
 
