18715e1bfd20b3d6e2cca1df25f895b3b31bc2b9
ChauPham90 <minhchaupham90@gmail.com>
2020-11-19 13:51:55 +0700

    remove font-end

--- font-end/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-function App() {
-  return <div className="App">hello from Big Mang Den</div>;
-}
-
-export default App;
